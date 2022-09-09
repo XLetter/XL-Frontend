45ef265df4a38535e947fba809dbacb8772b2f38
minkyung01 <min2000@ewhain.net>
2022-09-09 16:14:11 +0900

    0909 navigation bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import Home from './pages/index.js';
+import Original from './pages/original.js';
+import Series from './pages/series.js';
+import MyPage from './pages/mypage.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/original" element={<Original />} />
+        <Route path="/series" element={<Series />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Nav, NavLink, Bars, NavMenu, NavBtn, NavBtnLink } from "./NavbarElements";
+import logo from '../../assets/logo.png';
+import './NavbarElements.css';
+
+const Navbar = () => {
+  return (
+    <>
+        <Nav>
+            <NavLink to="/">
+                <img src={logo} classname="img" alt="logo" />
+            </NavLink>
+            <Bars />
+            <NavMenu>
+                <NavLink to="/" activeStyle>
+                    Home
+                </NavLink>
+                <NavLink to="/original" activeStyle>
+                    XL Original
+                </NavLink>
+                <NavLink to="/series" activeStyle>
+                    XL Series
+                </NavLink>
+                <NavLink to="/mypage" activeStyle>
+                    My Page
+                </NavLink>
+            </NavMenu>
+            <NavBtn>
+                <NavBtnLink to="search">Search</NavBtnLink>
+            </NavBtn>
+            <NavBtn>
+                <NavBtnLink to="profile">Profile</NavBtnLink>
+            </NavBtn>
+        </Nav>
+    </>
+  );
+};
+
+export default Navbar;
