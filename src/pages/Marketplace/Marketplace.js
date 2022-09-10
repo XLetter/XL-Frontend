848746ed5f724fa4cf348fa0cc637564b71416db
yujin0923 <ratias0923@ewhain.net>
2022-09-10 15:26:03 +0900

    Marketplace

--- src/pages/Marketplace/Marketplace.js
+++ src/pages/Marketplace/Marketplace.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './Marketplace.css';
+import { Nav, NavBtn, NavBtnLink } from "../../components/Navbar/NavbarElements";
+import Row from '../../Row/Row';
+import requests from '../../requests/requests';
+import Banner from './BannerMarketplace/bannermarketplace'
+function Marketplace(){
+  return(
+    <div className="marketplace">
+      <Banner />
+      <button>Search</button><button>My NFT</button>
+      <Row
+        title="실시간 거래작품"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow={true}
+     />
+     <Row title="작품별 Collection" fetchUrl={requests.fetchTrending} />
+      <Row title="최근 추가된 Collection" fetchUrl={requests.fetchTopRated} />
+      
 
-const Marketplace = () => {
-  return (
-    <div 
-        style={{
-            display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center',
-            height: '90vh'
-        }}
-    >
-        <h1>Marketplace</h1>
     </div>
+    
   );
-};
-
+}
 export default Marketplace;
