1c3155d4b562f3e50ac772cd33ac83a1bbd02f8d
skysky33 <101037954+skysky33@users.noreply.github.com>
2022-09-10 17:24:24 +0900

    Individual Page

---
+++ src/pages/Original/IndividualNovelPageOriginal.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './IndividualNovelPageOriginal.css';
+
+const IndividualNovelPageOriginal = () => {
+  return (
+    // <div className="IndividualNovelPageOriginal">
+    //   <div className="infoOfNovel">
+    //     <div id="readFirst">첫회보기</div>
+    //     <div id="xltCharge">XLT 충전하기</div>
+    //     <div />
+    //     <div className="favAndRanking">
+    //       <div className="fav">
+    //         <div id="favFigure"></div>
+    //         <p id="favName">즐겨찾기</p>
+    //       </div>
+    //       <div className="ranking">
+    //         <div id="rankingFigure"></div>
+    //         <p id="rankingName">나의 랭킹</p>
+    //       </div>
+    //     </div>
+    //     <h5 id="myNFT">내가 보유한 NFT</h5>
+    //   </div>
+    // </div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh',
+      }}
+    >
+      hello
+    </div>
+  );
+};
+
+export default IndividualNovelPageOriginal;
--- src/pages/Original/original.js
+++ src/pages/Original/original.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Original = () => {
   return (
-    <div 
-        style={{
-            display: 'flex', 
-            justifyContent: 'center', 
-            alignItems: 'center',
-            height: '90vh'
-        }}
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '90vh',
+      }}
     >
-        <h1>XL Original</h1>
+      <h1>XL Original</h1>
+      <br />
+      <NavLink to="IndividualNovelPageOriginal.js"> Individual Page</NavLink>
     </div>
   );
 };
