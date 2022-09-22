54ee4e6605fee288f326e04441e3a4df7e3e01b2
skysky33 <101037954+skysky33@users.noreply.github.com>
2022-09-22 09:16:41 +0900

    new

--- src/pages/Marketplace/NFTDetailPage.js
+++ src/pages/Marketplace/NFTDetailPage.js
@@ -4,14 +4,16 @@ import './NFTDetailPage.css';
 
 function NFTDetailPage() {
   return (
-    <div className="detailPage">
-      <img id="nftImage"></img>
-      <p id="nameWork">nameofwork</p>
-      <h2 id="nftName">nftName</h2>
-      <button id="readWork">readbutton</button>
-      <p id="nftPrice">nftprice</p>
-      <button id="buyNft">buynow</button>
-      <button id="makeOffer">makeoffer</button>
+    <div>
+      <div className="nftDetail">
+        <img id="nftImage"></img>
+        <p id="nameWork">nameofwork</p>
+        <h2 id="nftName">nftName</h2>
+        <button id="readWork">readbutton</button>
+        <p id="nftPrice">nftprice</p>
+        <button id="buyNft">buynow</button>
+        <button id="makeOffer">makeoffer</button>
+      </div>
     </div>
   );
 }
