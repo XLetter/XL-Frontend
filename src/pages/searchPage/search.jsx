4b87ad6af143130819cf918556d2dfb781c0ee41
yujin0923 <ratias0923@ewhain.net>
2022-09-26 10:21:20 +0900

    Update search.jsx

--- src/pages/searchPage/search.jsx
+++ src/pages/searchPage/search.jsx
@@ -1,11 +1,17 @@
 import axios from "../../axios/Axios";
 import React, {useEffect,useState}from "react";
 
+function axiosTest() {
+  return axios.get(fetchURL).then(response => response.data)
+}
+const fetchURL="https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&with_networks=213";
 const useSearchApi = (initialSearchKeyword) => {
     const [searchKeyword, setSearchKeyword] = useState(initialSearchKeyword);
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
+
+   
   
     useEffect(() => {
       const fetchData = async () => {
@@ -13,7 +19,7 @@ const useSearchApi = (initialSearchKeyword) => {
         setIsLoading(true);
   
         try {
-          const result = await axios(
+          const result = await axiosTest(
             `${process.env.API_HOST}/search?keyword=${searchKeyword}`
           );
           setData(result.data);
@@ -27,5 +33,6 @@ const useSearchApi = (initialSearchKeyword) => {
     }, [searchKeyword]);
   
     return [{ data, isLoading, isError }, setSearchKeyword];
-  };
+};
+  
   export default useSearchApi
