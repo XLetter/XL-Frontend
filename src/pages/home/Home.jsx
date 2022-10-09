9ca4487b5208a03739d38056468b0400a55296f9
skysky33 <101037954+skysky33@users.noreply.github.com>
2022-10-09 23:16:44 +0900

    Revert "Merge branch 'origin-yena-1008' into Merge3"
    This reverts commit 9eb706a33207e923a21b13e9f1dfe5d94a25d063, reversing
    changes made to e0f011cc5fcf46ebf3d5871bee2186f1273c7137.

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -8,27 +8,31 @@ function Home() {
   return (
     <div className="home">
       <div className="row_hm_out_1">
+      
         <Banner />
         <div className="row_hm_out_2">
-          <div className="row_hm">
-            <div className="row_hm_in">
-              <Row title="XL Original" fetchUrl={requests.fetchOriginal} isLargeRow={true} />
-            </div>
-
-            <div className="row_hm_in">
-              <Row title="XL Series" fetchUrl={requests.fetchSeries} isLargeRow={true} />
-            </div>
-
-            <div className="row_hm_in">
-              <Row title="Now Voting" fetchUrl={requests.fetchOriginal} isLargeRow={true} />
-            </div>
-
-            <div className="row_hm_in">
-              <Row title="Trending" fetchUrl={requests.fetchAll} isLargeRow={true} />
-            </div>
+        <div className="row_hm">
+          <div className="row_hm_in">
+            <Row title="XL Original" fetchUrl={requests.fetchOriginal} isLargeRow={true} />
+          </div>
+        
+        
+          <div className="row_hm_in">
+            <Row title="XL Series" fetchUrl={requests.fetchSeries} isLargeRow={true} />
+          </div>
+       
+        
+          <div className="row_hm_in">
+            <Row title="Now Voting" fetchUrl={requests.fetchOriginal} isLargeRow={true} />
+          </div>
+        
+       
+          <div className="row_hm_in">
+            <Row title="Trending" fetchUrl={requests.fetchAll} isLargeRow={true} />
           </div>
         </div>
       </div>
+    </div>
     </div>
   );
 }
